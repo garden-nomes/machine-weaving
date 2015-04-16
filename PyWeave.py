10caf89a72ce8473707dc6b666bd327d5f5b0011
thebareju <noahbweiner@gmail.com>
2015-04-16 16:12:28 -0700

    started on .wif file loading

--- PyWeave.py
+++ PyWeave.py
@@ -28,9 +28,42 @@ class Weave:
     ## alternate constructor that loads .WIF file
     
     def __init__(self, fileName):
-        ###
-        ## TODO
-        ###
+        #try:
+            # open file and check it's a .wif
+            file = open(fileName, 'r')
+            
+            if file.readline().strip() != "[WIF]":
+                raise
+
+            # read through lines
+            lines = file.readlines()
+            for i in range(0, len(lines)):
+                
+                # if line is the beginning of the color table
+                if lines[i].strip() == "[COLOR TABLE]":
+                    # fill color table
+                    self.colorTable = []
+                    for i in range(0, 10):
+                        self.colorTable.append("")
+                        
+                    # loop until we reach a '['
+                    j = i + 1
+                    while lines[j][0] != "[":
+                        # process line
+                        line = lines[j].strip().split("=")
+                        index = int(line[0])
+                        print(index)
+                        self.colorTable[index] = line[1].split(",")
+                        # read next line
+                        j += 1
+                        
+                    print(self.colorTable)
+                        
+            
+            file.close()
+        #except:
+        #    print("Error loading .WIF file!")
+        
     
     ## saves a bitmap image of the weave
     
@@ -39,7 +72,13 @@ class Weave:
     # spacing - amount of pixels to put in between each thread (default 0)
     
     def saveBitmap(self, fileName, scale = 1, spacing = 0):
+        print("Eventually this will save a bitmap!")
         ###
         ## TODO
         ###
+
+# akin to 'main' method, will only run if this specific file is run
+if __name__ == "__main__":
+    weave = Weave("test.wif")
+    weave.saveBitmap("output.bmp")
     
