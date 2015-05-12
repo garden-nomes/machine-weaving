8fbee2511351ca0ebe11d7831d938c3a17b3f525
Noah Weiner <noah@noahs-mbp.evergreen.edu>
2015-05-12 12:04:53 -0700

    It works!

--- wolfram.py
+++ wolfram.py
@@ -1,4 +1,7 @@
 import random
+from PyWeave import Weave
+
+## generate first (seed) row of a wolfram cellular automata
 
 def seed(width, probability = 0.5):
     pattern = []
@@ -11,6 +14,8 @@ def seed(width, probability = 0.5):
             pattern[0].append(False)
     
     return pattern
+
+## append a single row to the pattern using a set of rules
 
 def generate(pattern, rules):
     if len(rules) < 8:
@@ -47,19 +52,61 @@ def generate(pattern, rules):
         
         l = c
     
-    pattern.append(generation)
-    return pattern
+    return generation
+
+## create a weave using wolfram cellular automata
+
+def createWeave(width, height, rules):
+    # first generate the pattern
+    
+    pattern = seed(width)
+    for i in range(height - 1):
+        pattern.append(generate(pattern, rules))
+    
+    # set up weave
+    
+    colorTable = { 1: [0, 0, 0], 2: [255, 255, 255] }
+    warpThreads = width
+    weftThreads = height
+    
+    # fill weft and warp with black/white
+    
+    warpColors = []
+    for i in range(warpThreads):
+        warpColors.append(1)
+    
+    weftColors = []
+    for i in range(weftThreads):
+        weftColors.append(2)
+    
+    # to simulate a Jaquard loom, each thread has its own harness
+    
+    threading = []
+    for i in range(warpThreads):
+        threading.append(i + 1)
+    
+    # each tie-up is unique, so we generate the tie-up as we go
+    
+    tieUp = {}
+    treadling = []
+    
+    for row in range(len(pattern)):
+        harnesses = []
+        for x in range(len(pattern[row])):
+            if pattern[row][x]:
+                harnesses.append(x + 1)
+        tieUp[row] = harnesses
+        treadling.append(row)
+    
+    # create weave pattern
+    
+    weave = Weave(colorTable, warpThreads, weftThreads, tieUp,
+                  threading, treadling, warpColors, weftColors)
+    return weave
+    
+    
+    
 
 if __name__ == "__main__":
-    pattern = seed(128)
-    for i in range(128):
-        pattern = generate(pattern, [False, False, False, True, True, True, True, False])
-    
-    for row in pattern:
-        line = ""
-        for column in row:
-            if column:
-                line += "*"
-            else:
-                line += " "
-        print(line)
+    weave = createWeave(128, 128, [False, False, False, True, True, True, True, False])
+    weave.saveBitmap("wolfram.bmp")
