65355a04f5cca2cba99362c7bd8752f6fc63c878
elsobylak <elsobylak@users.noreply.github.com>
2015-05-12 10:18:02 -0700

    Update wifGen.py

--- wifGen.py
+++ wifGen.py
@@ -1,10 +1,18 @@
 # Relably creates random weaves that can be processed directly by machine-weaving pyweave
+# KNOWN ISSUES:
+#   -Only has one tieup pattern hard-wired in, so the range of design is limited
+
 
 # need random values for colors and number of warps and wefts
 import random
 
+import ProbWeave
+
+
 # lots of print statements that print out the headers and whatnot
 # then there is some filler lines that produce the actual information
+
+
 
 def wifGen():
 
@@ -14,12 +22,10 @@ def wifGen():
     file.write('[COLOR TABLE]''\n')
 
     # assign color1 three random ints between 0 and 255
-    color1 = str((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-    file.write('1='+color1.strip('()').replace(' ','')+'\n')
+    file.write('1='+str(ProbWeave.pickColor().strip('()').replace(' ',''))+'\n')
 
     # assign color2 three random ints for its color
-    color2 = str((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-    file.write('2='+color2.strip('()').replace(' ','')+'\n')
+    file.write('2='+str(ProbWeave.pickColor().strip('()').replace(' ',''))+'\n')
 
     file.write('[WARP]''\n')
     # give a random amount of warp threads
@@ -35,24 +41,28 @@ def wifGen():
 
 
     file.write('[TIEUP]''\n')
-    harness1 = (random.randint(1,4),random.randint(1,4))
+    harness1 = (random.randint(1,2),random.randint(1,2))
     file.write('1='+str(harness1).strip('()').replace(' ','')+'\n')
-    harness2 = (random.randint(1,4),random.randint(1,4))
+    harness2 = (random.randint(2,3),random.randint(2,3))
     file.write('2='+str(harness2).strip('()').replace(' ','')+'\n')
-    harness3 = (random.randint(1,4),random.randint(1,4))
+    harness3 = (random.randint(3,4),random.randint(3,4))
     file.write('3='+str(harness3).strip('()').replace(' ','')+'\n')
     harness4 = (random.randint(1,4),random.randint(1,4))
     file.write('4='+str(harness4).strip('()').replace(' ','')+'\n')
 
     # assign each thread in the warp to a tieup number
     file.write('[THREADING]''\n')
-    for x in range(threads1):
-        file.write(str(x)+'='+str(random.randint(1,4))+'\n')
+    for x in range(0, threads1, 2):
+        file.write(str(x)+'='+str(random.randint(1,3))+'\n')
+    for x in range(1, threads1, 2):
+        file.write(str(x)+'='+str(random.randint(2,4))+'\n')
 
     # assign each thread in the weft to a tieup number
     file.write('[TREADLING]''\n')
-    for x in range(threads2):
-        file.write(str(x)+'='+str(random.randint(1,4))+'\n')
+    for x in range(0, threads2, 2):
+        file.write(str(x)+'='+str(random.randint(1,3))+'\n')
+    for x in range(1, threads2, 2):
+        file.write(str(x)+'='+str(random.randint(2,4))+'\n')
 
     # assign warp threads to color1
     file.write('[WARP COLORS]''\n')
@@ -67,4 +77,4 @@ def wifGen():
     file.close()
 
 #run the script
-wifGen()
+#wifGen()
